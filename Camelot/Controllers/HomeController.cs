792ed158d84bc695ac49a694b97a98c7fd9106bb
ElPolloLoco00 <mijnbooks@gmail.com>
2017-01-17 15:54:47 +0100

    Adding login authentication
    #Add [Authorize] in HomeController
    
    #Add authentication mode in web.config

--- Camelot/Controllers/HomeController.cs
+++ Camelot/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace Camelot.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private CamelotContext db = new CamelotContext();
